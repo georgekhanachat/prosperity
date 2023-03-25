d9752dc612f5d6c88a0eabfba3b47c31df77c766
georgekhanachat <58150211+georgekhanachat@users.noreply.github.com>
2023-03-25 18:28:14 +0100

    Correlation algo - 5680 pnl

--- Trader.py
+++ Trader.py
@@ -1,67 +1,109 @@
-from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
+import json
+from collections import deque
+from math import isnan
+
+import numpy as np
+import pandas as pd
+from numpy import ndarray
+
+from datamodel import Order, ProsperityEncoder, Symbol, TradingState, OrderDepth
+from typing import Any, Dict, List
+
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]]) -> None:
+        print(json.dumps({
+            "state": state,
+            "orders": orders,
+            "logs": self.logs,
+        }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True))
+
+        self.logs = ""
+
+
+logger = Logger()
 
 
 class Trader:
 
-    def run(self, state: TradingState) -> Dict[str, List[Order]]:
-        """
-        Only method required. It takes all buy and sell orders for all symbols as an input,
-        and outputs a list of orders to be sent
-        """
-        # Initialize the method output dict as an empty dict
-        result = {}
+    def __init__(self):
+        self.dolphin_sightings_history = deque(maxlen=20)
+        self.price_history = {
+            'DIVING_GEAR': deque(maxlen=20)
+        }
 
-        # Iterate over all the keys (the available products) contained in the order depths
+    # @staticmethod
+    # def calculate_correlation(x: list, y: list) -> float:
+    #     """Calculate the correlation coefficient between two arrays."""
+    #     # Calculate the mean of each array
+    #     x_mean = np.mean(x)
+    #     y_mean = np.mean(y)
+    #
+    #     # Calculate the standard deviation of each array
+    #     x_std = np.std(x)
+    #     y_std = np.std(y)
+    #
+    #     # Calculate the covariance between the two arrays
+    #     covariance = np.sum((x - x_mean) * (y - y_mean)) / len(x)
+    #
+    #     # Calculate the correlation coefficient
+    #     correlation = covariance / (x_std * y_std)
+    #
+    #     return correlation
+
+    @staticmethod
+    def get_medium_price(order_depth: OrderDepth) -> ndarray:
+        """ Computes the medium price based on buy and sell orders """
+
+        buy_orders = [float(item) for item in order_depth.buy_orders]
+        sell_orders = [float(item) for item in order_depth.sell_orders]
+        prices = np.array([buy_orders + sell_orders])
+
+        return np.mean(prices)
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        result = {}
+        dolphin_sightings = state.observations.get("DOLPHIN_SIGHTINGS")
+        if dolphin_sightings is not None:
+            self.dolphin_sightings_history.append(dolphin_sightings)
+
         for product in state.order_depths.keys():
 
-            # Check if the current product is the 'PEARLS' product, only then run the order logic
-            if product == 'PEARLS':
-
-                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
+            if product == "DIVING_GEAR":
+                order_dg = []
                 order_depth: OrderDepth = state.order_depths[product]
+                self.price_history[product].append(self.get_medium_price(order_depth))
 
-                # Initialize the list of Orders to be sent as an empty list
-                orders: list[Order] = []
+                df = pd.DataFrame(
+                    {"DIVING_GEAR": self.price_history[product], "DOLPHIN_SIGHTINGS": self.dolphin_sightings_history})
+                corr = df.corr()["DIVING_GEAR"]["DOLPHIN_SIGHTINGS"]
+                logger.print(corr)
+                if not isnan(corr):
+                    if corr > 0.6 and state.position.get("DIVING_GEAR", 0) < 50:
+                        # Buy more DIVING_GEAR
+                        dg_price = min(state.order_depths["DIVING_GEAR"].sell_orders.keys())
+                        quantity = min(50 - state.position.get("DIVING_GEAR", 0),
+                                       state.order_depths["DIVING_GEAR"].sell_orders[dg_price])
+                        if (-quantity) + state.position[product] > 50:
+                            quantity = state.position[product] - 50
+                        logger.print("BUY DIVING_GEAR", dg_price, -quantity)
+                        order = Order("DIVING_GEAR", dg_price, -quantity)
+                        order_dg.append(order)
+                    elif corr < -0.6:
+                        # Sell DIVING_GEAR
+                        dg_price = min(state.order_depths["DIVING_GEAR"].buy_orders.keys())
+                        quantity = min(state.position.get("DIVING_GEAR", 0),
+                                       -state.order_depths["DIVING_GEAR"].buy_orders[dg_price])
+                        logger.print("SELL DIVING_GEAR", dg_price, quantity)
+                        order = Order("DIVING_GEAR", dg_price, quantity)
+                        order_dg.append(order)
+                result['DIVING_GEAR'] = order_dg
 
-                # Define a fair value for the PEARLS.
-                # Note that this value of 1 is just a dummy value, you should likely change it!
-                acceptable_price = 1
-
-                # If statement checks if there are any SELL orders in the PEARLS market
-                if len(order_depth.sell_orders) > 0:
-
-                    # Sort all the available sell orders by their price,
-                    # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
-
-                    # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    if best_ask < acceptable_price:
-
-                        # In case the lowest ask is lower than our fair value,
-                        # This presents an opportunity for us to buy cheaply
-                        # The code below therefore sends a BUY order at the price level of the ask,
-                        # with the same quantity
-                        # We expect this order to trade with the sell order
-                        print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(Order(product, best_ask, -best_ask_volume))
-
-                # The below code block is similar to the one above,
-                # the difference is that it finds the highest bid (buy order)
-                # If the price of the order is higher than the fair value
-                # This is an opportunity to sell at a premium
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_volume))
-
-                # Add all the above orders to the result dict
-                result[product] = orders
-
-                # Return the dict of orders
-                # These possibly contain buy or sell orders for PEARLS
-                # Depending on the logic above
+        logger.flush(state, result)
         return result
